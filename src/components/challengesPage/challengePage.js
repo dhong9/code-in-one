ad19ad7f0def448c0e26a49baa2ce9ceacdc1dc7
Daniel Hong <d_hong7@u.pacific.edu>
2021-11-30 18:49:20 -0800

    Ensured that useEffect is called once when loading challenges

--- src/components/challengesPage/challengePage.js
+++ src/components/challengesPage/challengePage.js
@@ -10,7 +10,7 @@ export default function ChallengePage() {
         fetch("http://127.0.0.1:8000/challenges/")
             .then(response => response.json())
             .then(data => setChallenges(data.results));
-    })
+    }, [])
 
     return (
         <React.Fragment>
