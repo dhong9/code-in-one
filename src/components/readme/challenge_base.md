833692ce48b9dca0515983339dbc75ea5c89d3cd
Daniel Hong <d_hong7@u.pacific.edu>
2021-12-11 15:38:55 -0800

    Added markdown parser

---
+++ src/components/readme/challenge_base.md
@@ -0,0 +1,8 @@
+### Single-Function (Codewriting)
+
+Implement the function in your preferred language. When run, your function is wrapped in a closure, so some declarations may result in a Syntax (Compilation) Error.
+
+Keep the following details in mind:
+* Do not change the name of the function/method. Changing its name will result in a syntax error.
+* You can write to stdout for debugging purposes; output will be displayed under each test case.
+* If the value of some input/output argument is a string that contains double quote characters (") it will be shown in the output with an additional symbol. E.g. `inputString = hello"world"hello` will be displayed in the console as `"hello"world"hello"`.
--- src/components/readme/readme.js
+++ src/components/readme/readme.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+import challenge_base from "./challenge_base.md";
 
 class Readme extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            base: ""
+        };
+    }
+
+    componentWillMount() {
+        fetch(challenge_base)
+            .then(response => response.text())
+            .then(text => 
+                this.setState({
+                    base: text
+                })    
+            );
+    }
 
     render() {
         return (
             <div>
-                <h6>Readme</h6>
+                <h2>README</h2>
+                <ReactMarkdown children={this.state.base} />
             </div>
         );
     }
